02c7428c5f04cff4b2369c29d4db054cedceeda2
Graciela Cesareo Cruz <gracielacesareocruz@Gracielas-MacBook-Pro.local>
2017-05-04 23:09:18 -0500

    primera funcinalidad del nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /*
  * Archivo principal de JS
  */
+
+ var nav = document.getElementById('navegacion');
+ var titulo = document.getElementById('bootstrap');
+
+ document.addEventListener('scroll',estiloNav);
+
+ function estiloNav() {
+
+   if (window.scrollY > 90) {
+     nav.style.padding = "0px";
+     titulo.style.fontSize = "21px";
+   } else {
+     nav.style.padding = "10px";
+     titulo.style.fontSize = "28px";
+   }
+ }
